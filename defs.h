4e68456fc6945b5ae013b2cbfbab64697fe29537
joaogpd <joaogdrumond@hotmail.com>
2023-09-03 00:10:01 -0300

    Create defs.h

---
+++ defs.h
@@ -0,0 +1,19 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#define NEOPIXELPINRIGHT 6 
+#define NEOPIXELPINLEFT 7
+#define PIRPIN 2
+#define RXPIN 4
+#define TXPIN 5
+#define PIN2 0
+#define PIN3 1
+#define NUMPIXELS 1
+#define NUMSONGS 11
+#define UNUSED 0
+#define clearBit(reg, bit) (reg &= ~(1 << bit))
+#define ISCx1(x) ISC ## x ## 1
+#define ISCx0(x) ISC ## x ## 0
+#define INTx(x) INT ## x
+
+#endif
