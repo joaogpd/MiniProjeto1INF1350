68d4f7b444d7405dafb88b779b1ef098fb3da4c1
joaogpd <joaogdrumond@hotmail.com>
2023-09-05 19:21:31 -0300

    Add files via upload

--- defs.h
+++ defs.h
@@ -1,34 +1,27 @@
-/***************************************************************************
-*
-*  INF1350 - Sistemas Reativos
-*  Prof.: Adriano Branco
-*  Monitor: Pablo Nascimento
-*  Semestre: 2023.2
-*  Grupo 1: Cristiane Guimarães
-*           João Gabriel Peixoto
-*           Luiz Fernando Costa
-*
-*  PROJETO 1 - GISTEIN
-*
-***************************************************************************/
-
-
 #ifndef DEFS_H
 #define DEFS_H
 
+/**> Pino dos LEDs NeoPixel do olho direito */
 #define NEOPIXELPINRIGHT 6 
+/**> Pino dos LEDs NeoPixel do olho esquerdo */
 #define NEOPIXELPINLEFT 7
+/**> Pino do sensor PIR */
 #define PIRPIN 2
-#define RXPIN 4
-#define TXPIN 5
+/**> Pino do TX do DFMini Player (RX para o Arduino) */
+#define RXPIN 10
+/**> Pino do RX do DFMini Player (TX para o Arduino) */
+#define TXPIN 11
+/**> Valor da interrupção referente ao pino 2 (INT0) */
 #define PIN2 0
+/**> Valor da interrupção referente ao pino 3 (INT1) */
 #define PIN3 1
-#define NUMPIXELS 1
-#define NUMSONGS 11
+/**> Número de LEDS NeoPixel em cada pino */
+#define NUMPIXELS 2
+/**> Número de músicas no cartão SD */
+#define NUMSONGS 46
+/**> Valor usado para ignorar o parâmetro de argumento da biblioteca SchedulerVTimer */
 #define UNUSED 0
+/**> Macro que limpa um bit de um valor recebido */
 #define clearBit(reg, bit) (reg &= ~(1 << bit))
-#define ISCx1(x) ISC ## x ## 1
-#define ISCx0(x) ISC ## x ## 0
-#define INTx(x) INT ## x
 
 #endif
